7958d3cb283b6b7630e908ad30b91484f35814cd
P__.mp4 <postlist.ed@gmail.com>
2022-08-07 20:03:08 +0900

    2022-08-07 최초 commit

---
+++ src/main/java/com/pmp4/websocketchat/WebSocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.pmp4.websocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmp4/websocketchat/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmp4.websocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
