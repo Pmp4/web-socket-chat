79d5e3742ef25c7afec371cea8c1fed2d1dddac9
P__.mp4 <postlist.ed@gmail.com>
2022-08-07 20:06:31 +0900

    2022-08-07 20:26 React 설치

--- src/main/java/com/pmp4/websocketchat/WebSocketChatApplication.java
+++ src/main/java/com/pmp4/websocketchat/WebSocketChatApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSocketChatApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebSocketChatApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketChatApplication.class, args);
+	}
 
 }
